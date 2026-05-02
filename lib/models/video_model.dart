fe806d455fe68e29985efb9b11608b6bb4b28305
Useless8000B <adrielluiz73@gmail.com>
2026-05-02 11:50:29 -0300

    Add VideModel

---
+++ lib/models/video_model.dart
@@ -0,0 +1,17 @@
+class VideoModel {
+  final String thumbnail;
+  final String title;
+  final String channelName;
+  final String views;
+  final String postedTime;
+  final String channelAvatar;
+
+  VideoModel({
+    required this.thumbnail,
+    required this.title,
+    required this.channelName,
+    required this.views,
+    required this.postedTime,
+    required this.channelAvatar,
+  });
+}
